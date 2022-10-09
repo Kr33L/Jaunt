c5d8d4b80f52f7c62ba0cd4ffb25237833456de9
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-09 12:09:44 +0100

    fs

---
+++ Source/Dialogue.cs
@@ -0,0 +1,35 @@
+﻿namespace Jaunt
+{
+    class Dialogue
+    {
+        public static string[] titleBanner = new string[]
+        {
+            " ┌┐       ┌┐ ",
+            " │├─┐┌┬┬─┬┤└┐",
+            "┌┤│┼└┤│││││┌┤",
+            "└─┴──┴─┴┴─┴─┘",
+               " *   ",
+               "*  * ",
+               "  * *",
+               " * * ",
+               "*   *",
+               " * * ",
+               "  *  ",
+               " * * ",
+               "  *  ",
+               "   * ",
+               "  *  ",
+        };
+
+        public static string[] chapterOne = new string[]
+        {
+               "Chapter",
+               "  One  ",
+
+        };
+
+        public static string ch1_01 = "{0}Walking along the forest path,\n" +
+                                      "You notice a {1}.";
+
+    }
+}
